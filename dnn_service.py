5c735c329a7e293debecb7b17228bad658e66bfc
João Paulo Barraca <jpbarraca@gmail.com>
2018-07-26 01:34:10 +0100

    Updates

--- dnn_service.py
+++ dnn_service.py
@@ -14,12 +14,6 @@ import tempfile
 import os
 import json
 
-confidence_threshold = 0.5
-
-def callback(pos):
-    global confidence_threshold
-    confidence_threshold = pos / 100.0
-
 class DNNService(object):
     
     def __init__(self, net, classes, outputs_names):
@@ -29,30 +23,41 @@ class DNNService(object):
 
     @cherrypy.expose
     def index(self):
-        return '<html><body><form action="/process" method="post" enctype="multipart/form-data">File: <input type="file" name="img"></input><br />Threshold: <input type="text" name="thr" value="0.5"></input><input type="submit" value="Process"></input></form></body></html>'
+        return '<html><body><form action="/process" method="post" enctype="multipart/form-data">File: <input type="file" name="img"></input><br />Score Threshold: <input type="text" name="thr_score" value="0.5"><br />NMS Threshold: <input type="text" name="thr_nms" value="0.4"></input><input type="submit" value="Process"></input></form></body></html>'
 
     @cherrypy.expose
-    def process(self, img, thr=0.5):
+    def process(self, img, thr_score=0.5, thr_nms=0.4, size=320):
         global confidence_threshold
         
         fd, path = tempfile.mkstemp()
+
         with os.fdopen(fd, "wb") as tmp:
-            size = 0
+            rsize = 0
             while True:
                 data = img.file.read(8192)
-                size += len(data)
+                rsize += len(data)
                 if not data:
                     break
 
-                if size > 20000000:
+                if rsize > 20000000:
                     tmp.close()
                     raise cherrypy.HTTPError(status=400)
 
                 tmp.write(data)
             tmp.close()
         try:
-            confidence_threshold = float(thr)
+            if isinstance(thr_score, str):
+                thr_score = float(thr_score)
+
+            if isinstance(thr_nms, str):
+                thr_nms = float(thr_nms)
+            
+            if isinstance(thr_size, str):
+                size = int(size)
         except:
+            raise cherrypy.HTTPError(status=400)
+
+        if thr_score < 0 or thr_score > 1 or thr_nms < 0 or thr_nms > 1 or size not in [320, 416, 608]: 
             raise cherrypy.HTTPError(status=400)
 
         cap = cv.VideoCapture(path)
@@ -63,50 +68,52 @@ class DNNService(object):
         if not has_frame:
             raise cherrypy.HTTPError(status=400)
 
-        blob = cv.dnn.blobFromImage(frame, 1/255, (608, 608), 0, True, crop=False)
+        frame_width = frame.shape[0]
+        frame_height = frame.shape[1]
 
-        net.setInput(blob)
+        blob = cv.dnn.blobFromImage(frame, 1/255, (size, size), 0, True, crop=False)
+
        
         tstart = time.time()
+        net.setInput(blob)
         outs = net.forward(self.outputs_names)
         tend= time.time()
 
         print('Inference time: %.2f s' % (tend - tstart))
 
-        result = self.postprocess(net, classes, frame, outs)
+        result = self.postprocess(net, classes, frame, outs, thr_score, thr_nms, frame_width, frame_height)
+
         cherrypy.response.headers['Content-Type'] = 'text/json'
         
         return json.dumps(result, indent=4)        
         
-    def postprocess(self, net, classes, frame, outs):
+    def postprocess(self, net, classes, frame, outs, thr_score, thr_nms, frame_width, frame_height):
         predictions = []
-        frame_width = frame.shape[0]
-        frame_height = frame.shape[1]
 
         layer_names = net.getLayerNames()
         last_layer_id = net.getLayerId(layer_names[-1])
         last_layer = net.getLayer(last_layer_id)
 
-        if last_layer.type == 'Region':
-            classIds = []
-            confidences = []
-            boxes = []
-            for out in outs:
-                for detection in out:
-                    scores = detection[5:]
-                    classId = np.argmax(scores)
-                    confidence = scores[classId]
-                    if confidence > confidence_threshold:
-                        center_x = int(detection[0] * frame_width)
-                        center_y = int(detection[1] * frame_height)
-                        width = int(detection[2] * frame_width)
-                        height = int(detection[3] * frame_height)
-                        left = center_x - width / 2
-                        top = center_y - height / 2
-                        classIds.append(classId)
-                        confidences.append(float(confidence))
-                        boxes.append([left, top, width, height])
-            indices = cv.dnn.NMSBoxes(boxes, confidences, confidence_threshold, 0.4)
+        classIds = []
+        confidences = []
+        boxes = []
+
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                if confidence > thr_score:
+                    center_x = int(detection[0] * frame_width)
+                    center_y = int(detection[1] * frame_height)
+                    width = int(detection[2] * frame_width)
+                    height = int(detection[3] * frame_height)
+                    left = center_x - width / 2
+                    top = center_y - height / 2
+                    classIds.append(classId)
+                    confidences.append(float(confidence))
+                    boxes.append([left, top, width, height])
+            indices = cv.dnn.NMSBoxes(boxes, confidences, thr_score, thr_nms)
 
             for i in indices:
                 i = i[0]
